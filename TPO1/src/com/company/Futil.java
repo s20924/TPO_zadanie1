8297db0120f9e7889bec697a809f087839b56b9e
Patryk Popiołek <s20924@pjwstk.edu.pl>
2022-03-06 14:13:17 +0100

    aaa

--- TPO1/src/com/company/Futil.java
+++ TPO1/src/com/company/Futil.java
@@ -1,4 +1,17 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class Futil {
+    public static void processDir(String dirName, String resultFileName) throws IOException {
+        Path path = Paths.get(dirName);
+        Files.walkFileTree(path,new SimpleFileVisitor<Path>() {
+            public FileVisitResult visitFIle(Path file, BasicFileAttributes a) throws IOException {
+                System.out.println(Files.readAllLines(file));
+            }
+            return FileVisitResult.CONTINUE;
+        });
+    }
 }
--- TPO1/src/com/company/Main.java
+++ TPO1/src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String dirName = System.getProperty("user.home") + "/TPO1dir";
         String resultFileName = "TPO1res.txt";
         Futil.processDir(dirName, resultFileName);
